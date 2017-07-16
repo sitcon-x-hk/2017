9e589f7daeeb26625fa90ba3ef7dbe686808419a
Tony Yip <tony@opensource.hk>
2017-07-17 02:24:36 +0800

    Fix assets path

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: '',
-    assetsPublicPath: '/2017',
+    assetsPublicPath: '/2017/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -26,7 +26,7 @@ module.exports = {
     port: 8080,
     autoOpenBrowser: false,
     assetsSubDirectory: '',
-    assetsPublicPath: '/2017',
+    assetsPublicPath: '/2017/',
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
