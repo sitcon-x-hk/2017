42362ef67bbfdc024b5d54ec2b6517785bbfe43e
Tony Yip <tony@opensource.hk>
2017-07-17 02:44:35 +0800

    Fix wrong year

--- src/components/home/Jumbotron.vue
+++ src/components/home/Jumbotron.vue
@@ -8,7 +8,7 @@
       <p class="white--text">
         SITCON Student IT Conference, formed by students with passion in IT,
         a student-centered organisation hope to provide a stage for all Hong Kong students to express and develop their talent with Open Source.
-        The theme of year 2016 is "Exploration and Innovation", hoping to help students finding their way in IT industry.
+        The theme of year 2017 is "Exploration and Innovation", hoping to help students finding their way in IT industry.
       </p>
     </v-layout>
   </div>
