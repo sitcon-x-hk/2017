68edfc119c4050b74d59768114e56cff9f0f8331
Tony Yip <tony@opensource.hk>
2017-07-16 18:06:11 +0800

    Change home main card

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <v-parallax src="/2017/img/background.jpg">
-      <v-layout column align-center justify-center>
+    <div class="backdrop">
+      <div class="background">
+        <img src="/2017/img/background.jpg" />
+      </div>
+      <v-layout column align-center justify-center class="content">
         <h2 class="white--text">SITCON x HK 2017</h2>
         <p class="white--text">
           SITCON Student IT Conference, formed by students with passion in IT,
           a student-centered organisation hope to provide a stage for all Hong Kong students to express and develop their talent with Open Source.
-          The theme of year 2016 is "Let's Open Source", hoping to join students in the world of Open Source.
+          The theme of year 2016 is "Exploration and Innovation", hoping to help students finding their way in IT industry.
         </p>
       </v-layout>
-    </v-parallax>
+    </div>
     <v-card class="blue-grey darken-2 white--text">
       <v-card-title primary-title>
         <div class="headline">Register for event</div>
@@ -31,5 +34,41 @@
       p
         text-align: center
         font-size: 1.5rem
+  .backdrop
+    position: relative
+    overflow: hidden
+    background-repeat: no-repeat
+    padding: 0
+    margin: 0
+    z-index: 0
+    height: 500px
+    margin-bottom: 1rem
+
+    .content
+      display: flex
+      flex-direction: column
+      color: #fff
+      height: 100%
+      z-index: 2
+      position: relative
+      justify-content: center
+      padding: 0 1rem
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0
+      z-index: 1
+
+      img
+        position: absolute
+        bottom: 0
+        left: 50%
+        min-width: 100%
+        min-height: 100%
+        display: block
+        transform: translate3d(-50%, 214px, 0px)
+        z-index: 1
 </style>
 
