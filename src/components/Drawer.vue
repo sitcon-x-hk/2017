0e5dd43154b254a1c3e81e60c70cba67b10b730e
Tony Yip <tony@opensource.hk>
2017-07-17 16:11:50 +0800

    Fix style in active link

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -2,9 +2,10 @@
   <v-navigation-drawer
         temporary
         v-model="drawer"
+        dark
   >
     <v-list dense>
-      <v-list-tile :to="nav.link" v-for="nav in links" :key="nav.display">
+      <v-list-tile :to="nav.link" v-for="nav in links" :key="nav.display" class="drawer-tile">
         <v-list-tile-content>
           {{ nav.display }}
         </v-list-tile-content>
@@ -16,8 +17,7 @@
 <style scoped lang="sass">
   @import color
 
-  .router-link-exact-active
-    color: lighten($theme-red, 5%) !important
+  .router-link-exact-active .list__tile__content
     font-weight: bold
 </style>
 
