4371ba70ce73c15868f39404d9bc2611450f93a2
Tony Yip <tony@opensource.hk>
2017-07-20 22:03:57 +0800

    Change Drawer toggle colour

--- src/components/toolbar/Component.vue
+++ src/components/toolbar/Component.vue
@@ -2,7 +2,7 @@
   <div id="toolbar" :class="toolbarClass">
     <nav>
       <v-btn icon @click.native="openDrawer" v-if="isMobile">
-        <v-icon>menu</v-icon>
+        <v-icon class="primary--text">menu</v-icon>
       </v-btn>
       <div class="title hidden-md-and-up">
         <router-link :to="{name: 'home'}" data-role="brand">SITCON x HK 2017</router-link>
