4b5ecc6833b9bf39ea860e49467c5f610357e96a
Tony Yip <tony@opensource.hk>
2017-07-20 23:03:08 +0800

    Merge footer

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,6 @@
       <router-view v-else />
     </main>
     <footer-social />
-    <v-footer>
-      <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-sa/3.0/hk/">
-        <img alt="Creative Commons Licence" src="https://licensebuttons.net/l/by-sa/3.0/hk/88x31.png">
-      </a>
-      This work is licensed under a CC BY-SA 3.0 Hong Kong License . Logos and trademarks belong to their respective owners.
-    </v-footer>
   </v-app>
 </template>
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,10 +8,19 @@
         <v-icon class="wood-red--text">email</v-icon>
       </v-btn>
     </v-card-actions>
+    <v-card-text>
+      <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-sa/3.0/hk/">
+        <img alt="Creative Commons Licence" src="https://licensebuttons.net/l/by-sa/3.0/hk/88x31.png">
+      </a>
+      <br>
+      This work is licensed under a CC BY-SA 3.0 Hong Kong License.
+      <wbr>
+      Logos and trademarks belong to their respective owners.
+    </v-card-text>
   </v-card>
 </template>
 
 <style scoped lang="sass">
-  .card
-    background-color: rgba(0, 0, 0, .1) !important
+  .card__text
+    text-align: center
 </style>
