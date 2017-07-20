9ea86e7e81a171a763c6dc74868b45d967c6852e
Tony Yip <tony@opensource.hk>
2017-07-20 23:57:50 +0800

    Migrate to Google Map API instead of iframe

--- src/components/venue/Page.vue
+++ src/components/venue/Page.vue
@@ -2,21 +2,49 @@
   <v-card light>
     <v-card-title primary-title>
       <h3 class="headline black--text">Venue</h3>
+      4/F, Purple Zone, Academic 1, City University of Hong Kong Academic 1
     </v-card-title>
     <v-card-text>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.470207722735!2d114.17105940005355!3d22.335867685233016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x340407340422f691%3A0xccd88db952178b6b!2sCity+University+Of+Hong+Kong+Academic+1%2C+Kowloon+Tong!5e0!3m2!1sen!2shk!4v1471982302809"
-        width="600"
-        height="450"
-        frameborder="0"
-        style="border:0"
-        allowfullscreen
-      ></iframe>
+
+      <gmap-map
+        :center="center"
+        :zoom="zoom"
+      >
+        <gmap-marker
+          :position="center"
+          cliclable
+        />
+      </gmap-map>
     </v-card-text>
   </v-card>
 </template>
 
 <style lang="sass" scoped>
-  .card__text
-    text-align: center
+  .vue-map-container
+    width: 100%
+    height: 500px
 </style>
+
+<script>
+  import Vue from 'vue';
+  import * as VueGoogleMaps from 'vue2-google-maps';
+
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: process.env.GOOGLE_API_KEY,
+    },
+    installComponents: true,
+  });
+
+  export default {
+    data() {
+      return {
+        center: {
+          lat: 22.337609,
+          lng: 114.171540,
+        },
+        zoom: 15,
+      };
+    },
+  };
+</script>
