dc16ce9d615e59e7bc4408cf2b87c6c3175f9545
Tony Yip <tony@opensource.hk>
2017-07-31 05:23:10 +0800

    Update for google analytics

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,11 @@
 export const TRIGGER_DRAWER = 'TRIGGER_DRAWER';
+export const UPDATE_DRAWER = 'UPDATE_DRAWER';
 
 export default {
   [TRIGGER_DRAWER](state) {
     state.drawer = !state.drawer;
   },
+  [UPDATE_DRAWER](state, value) {
+    state.drawer = value;
+  }
 };
