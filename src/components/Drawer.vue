acab0402e8c8599780fcf2d4b055687374866d41
Tony Yip <tony@opensource.hk>
2017-08-01 19:17:54 +0800

    Display eventbrite register

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -10,11 +10,11 @@
           {{ nav.display }}
         </v-list-tile-content>
       </v-list-tile>
-      <!--<v-list-tile href="#" tag="a" class="ticket">-->
-        <!--<v-list-tile-content>-->
-          <!--Get Ticket-->
-        <!--</v-list-tile-content>-->
-      <!--</v-list-tile>-->
+      <v-list-tile tag="a" class="ticket" target="_blank" href="https://sitcon-x-hk-2017.eventbrite.com/?aff=website&utm_source=website&utm_medium=website&utm_campaign=drawer&utm_content=register">
+        <v-list-tile-content>
+          Get Ticket
+        </v-list-tile-content>
+      </v-list-tile>
     </v-list>
   </v-navigation-drawer>
 </template>
