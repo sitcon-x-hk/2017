7d2b8b0b4a01b6e912f9ce43bd66e64443a8d16b
Tony Yip <tony@opensource.hk>
2017-09-13 00:22:42 +0800

    Add Sponsor List

---
+++ src/components/sponsor/script.js
@@ -0,0 +1,14 @@
+export default {
+  data() {
+    return {
+      sponsors: {
+        diamond: [
+          {
+            name: 'Microsoft',
+            logo: '/2017/img/sponsor/microsoft.png',
+          },
+        ],
+      },
+    };
+  },
+};
