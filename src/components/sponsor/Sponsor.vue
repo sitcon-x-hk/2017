7817db81255872774775e4cd8edfe08d08f8c862
Tony Yip <tony@opensource.hk>
2017-09-13 15:46:06 +0800

    Add Hypebeast sponsor

--- src/components/sponsor/Sponsor.vue
+++ src/components/sponsor/Sponsor.vue
@@ -20,16 +20,35 @@
                 </v-flex>
                 <v-flex xs12 lg9>
                   <p>
-                    Microsoft Student Partner (MSP) Program has been developed since 2001 with 3,000+ student partners from over 100 countries.
-                    Our vision is to build a Microsoft community in campus for students
-                    with technical background who are passionate to technology, and cultivate them as technopreneurs.
-                    MSPs are Technopreneurs to explore wide and deep throughout the realm of IT to bring on impact
-                    and digital transformation to vitalize every walks of life.
-                    This year, we have a wide range of activities to transform and accelerate for students’ success,
-                    including: Hackathon Contests, Tech/Coffee Series, Insider Technical Training Workshops,
-                    olutions Building Projects, Microsoft Partners Event & Entrepreneurship Acceleration Series.
-                    With all these initiatives, students will be able to develop business acumen
-                    while gearing up their technical skills to map to business reality in IT.
+                    {{ sponsor.intro }}
+                  </p>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container class="mt-0">
+      <h4 class="headline">Bronze Sponsor</h4>
+      <v-layout row>
+        <v-flex xs12 lg6>
+          <v-card v-for="sponsor in sponsors.bronze">
+            <v-container grid-list-lg>
+              <v-layout row class="hidden-lg-and-up">
+                <v-avatar tile size="200">
+                  <img :src="sponsor.logo" class="avatar-img">
+                </v-avatar>
+              </v-layout>
+              <v-layout row>
+                <v-flex lg3 class="hidden-md-and-down">
+                  <v-avatar tile size="200">
+                    <img :src="sponsor.logo" class="avatar-img">
+                  </v-avatar>
+                </v-flex>
+                <v-flex xs12 lg9>
+                  <p>
+                    {{ sponsor.intro }}
                   </p>
                 </v-flex>
               </v-layout>
--- src/components/sponsor/script.js
+++ src/components/sponsor/script.js
@@ -6,6 +6,31 @@ export default {
           {
             name: 'Microsoft',
             logo: '/2017/img/sponsor/microsoft.png',
+            intro: `
+                    Microsoft Student Partner (MSP) Program has been developed since 2001 with 3,000+ student partners from over 100 countries.
+                    Our vision is to build a Microsoft community in campus for students
+                    with technical background who are passionate to technology, and cultivate them as technopreneurs.
+                    MSPs are Technopreneurs to explore wide and deep throughout the realm of IT to bring on impact
+                    and digital transformation to vitalize every walks of life.
+                    This year, we have a wide range of activities to transform and accelerate for students’ success,
+                    including: Hackathon Contests, Tech/Coffee Series, Insider Technical Training Workshops,
+                    olutions Building Projects, Microsoft Partners Event & Entrepreneurship Acceleration Series.
+                    With all these initiatives, students will be able to develop business acumen
+                    while gearing up their technical skills to map to business reality in IT.
+                    `,
+          },
+        ],
+        bronze: [
+          {
+            name: 'HYPEBEAST',
+            logo: '/2017/img/sponsor/hypebeast.png',
+            intro: `
+            HYPEBEAST was launched in 2005 as an one-stop news source for internet savvy fashion enthusiasts
+            and continues to inform and inspire trendsetters around the world.
+            The growth of HYPEBEAST has seen its focus transcend from its initial roots to encompass all forms
+            of fashion, arts, design and culture, including women’s fashion and trend site POPBEE and HYPEBAE,
+            the online destination for contemporary music. 
+            `,
           },
         ],
       },
