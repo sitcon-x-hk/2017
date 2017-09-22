23738c24e63bef8fbde46c917cd91d4a8f879edf
Tony Yip <tony@opensource.hk>
2017-09-22 14:41:11 +0800

    Add fixed hashtag on page

---
+++ src/components/Hashtag.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="hashtag" class="primary--text">
+    <span>#SITCONxHK</span>
+    <span>#SITCONxHK2017</span>
+  </div>
+</template>
+
+<style scoped lang="stylus">
+  #hashtag
+    position fixed
+    bottom 10px
+    left 20px
+    padding: 5px 10px
+    background-color rgba(255, 255, 255, 0.87)
+
+    @media screen and (max-width 600px)
+      font-size: 1.2rem
+</style>
